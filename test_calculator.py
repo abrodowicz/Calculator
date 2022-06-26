d6b6bf66280c21870727958d3caabc02553cb50f
Agnieszka Brodowicz <agnieszka.brodowicz@brainly.com>
2022-06-26 14:13:52 +0200

    Unitetsts file update

--- test_calculator.py
+++ test_calculator.py
@@ -4,37 +4,113 @@ import unittest
 
 class TestCalculator(unittest.TestCase):
 
-    # want write test for getNumber function but don't know how to handle variable_alias
-    def test_first_input_validation(self):
-        result = calculator.add
-        self.assertRaises(TypeError, result, "four", 2)
+    #Check if calculator greet an user/ doesn't work :(
+    def test_greeting_display(self):
+        greeting = calculator.welcome
+        self.assertEqual(greeting, "Hello! This is your calculator.")
 
-    def test_second_input_validation(self):
-        result = calculator.add
-        self.assertRaises(TypeError, result, 4, "two")
+    #Check if calculator provide an instruction
+    ?
 
-    # not sure about that one
-    def test_operator_validation(self):
-        operator = calculator.getOperator
-        self.assertRaises(TypeError, operator, "four")
+    # Check input validation -> want to write a test for getNumber function but don't know how to handle variable_alias
+    ?
 
-    def test_add_method(self):
+    #Check operator validation- positive scenario
+    ?
+
+    #Check operator validation- negative scenario
+    ?
+
+    #Check the addition of two positive integers- positive scenario
+    def test_add_method_1(self):
         result = calculator.add(10, 2)
         self.assertEqual(result, 12)
 
-    def test_subtract_method(self):
+    #Check the addition of two negative integers- positive scenario
+    def test_add_method_2(self):
+        result = calculator.add(-10, -2)
+        self.assertEqual(result, -12)
+
+    #Check the addition of one positive and one negative integer- positive scenario
+    def test_add_method_3(self):
+        result = calculator.add(2, -10)
+        self.assertEqual(result, -8)
+
+    #Check the addition of two integers- negative scenario
+    def test_add_method_4(self):
+        result = calculator.add(-2, 10)
+        self.assertNotEqual(result, 10)
+
+    #Check the subtraction of two positive integers- positive scenario
+    def test_subtract_method_1(self):
         result = calculator.subtract(10, 5)
         self.assertEqual(result, 5)
 
-    def test_multiply_method(self):
+    #Check the subtraction of two negative integers- positive scenario
+    def test_subtract_method_2(self):
+        result = calculator.subtract(-10, -5)
+        self.assertEqual(result, -5)
+
+    #Check the subtraction of one positive and one negative integer- positive scenario
+    def test_subtract_method_3(self):
+        result = calculator.subtract(10, -5)
+        self.assertEqual(result, 15)
+
+    #Check the subtraction of two integers- negative scenario
+    def test_subtract_method_4(self):
+        result = calculator.subtract(10, -5)
+        self.assertNotEqual(result, 5)
+
+    #Check the multiplication of two positive integers- positive scenario
+    def test_multiply_method_1(self):
         result = calculator.multiply(10, 3)
         self.assertEqual(result, 30)
 
-    def test_divide_method(self):
+    #Check the multiplication of two negative integers- positive scenario
+    def test_multiply_method_2(self):
+        result = calculator.multiply(-10, -3)
+        self.assertEqual(result, 30)
+
+    #Check the multiplication of one positive and one negative integer- positive scenario
+    def test_multiply_method_3(self):
+        result = calculator.multiply(10, -3)
+        self.assertEqual(result, -30)
+
+    #Check the multiplication of two integers- negative scenario
+    def test_multiply_method_4(self):
+        result = calculator.multiply(10, 3)
+        self.assertNotEqual(result, 8)
+
+    #Check the division of two positive  integers- positive scenario
+    def test_divide_method_1(self):
         result = calculator.divide(30, 3)
         self.assertEqual(result, 10)
 
-    # this one doesn't work :(
+    #Check the division of two negative integers- positive scenario
+    def test_divide_method_2(self):
+        result = calculator.divide(-30, -3)
+        self.assertEqual(result, 10)
+
+    #Check the division of one positive and one negative integer- positive scenario
+    def test_divide_method_3(self):
+        result = calculator.divide(30, -3)
+        self.assertEqual(result, -10)
+
+    #Check the division of two integers- negative scenario
+    def test_divide_method_4(self):
+        result = calculator.divide(30, 3)
+        self.assertNotEqual(result, 5)
+
+    #Check the division of a number by 0/ this one doesn't work :(
     def test_divide_by_zero_method(self):
         result = calculator.divide(10, 0)
         self.assertRaises(ZeroDivisionError, result)
+
+    #Check if result is rounded to 3 decimals
+    ?
+
+    #Check if user can exit calculator by typing “q”
+    ?
+
+    #Check if calculator says goodbye to an user
+    ?
